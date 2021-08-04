2f2778161bf6a6ffbab0f44953500619a01a4dff
sanreee <47985111+sanreee@users.noreply.github.com>
2021-08-04 12:49:26 +0300

    feed search and proxy

--- cbc-cli.py
+++ cbc-cli.py
@@ -51,7 +51,7 @@ def parse_config():
         return config
 
 config = parse_config()
-#parser = build_cli_parser("List devices")
+
 parser = ParserClass(formatter_class=SmartFormatter)
 parser.add_argument("instance", help="Instance name")
 
@@ -61,6 +61,8 @@ parser.add_argument("-i", help="Interactive mode", action='store_true')
 parser.add_argument("-e", help="Enriched mode. Fetch enriched events. Contains more post-processed data, including process links.", action='store_true')
 parser.add_argument("-ho", help="Hostname to search", default="*", dest='device_name')
 parser.add_argument("-st", help="Time window. y=year, w=week, d=day, h=hour, m=minute, s=second", default="4w", dest='timewindow')
+parser.add_argument("-x", help="HTTPS proxy :: e.g. -x 127.0.0.1:8080 - sorry no http :)", default=None, dest='proxy')
+parser.add_argument("-f", help="Feed search mode :: usage: -f feed.json :: cbfeeds format (github.com/carbonblack/cbfeeds)", default=None)
 
 args = parser.parse_args()
 passwd = getpass.getpass(prompt='Password for KeePass database: ')
@@ -69,12 +71,20 @@ kp = PyKeePass(keepass_path, password=passwd)
 instance = args.instance
 timewindow = args.timewindow
 
+global feedMode
 
 colors = {
         'blue': '\033[94m',
         'pink': '\033[95m',
         'green': '\033[92m',
         }
+
+if args.proxy:
+  proxies = {
+    "https": args.proxy
+  }
+else: 
+  proxies = None
 
 temp = None
 sweepMode = False
@@ -94,7 +104,7 @@ def colorize(string, color):
 
 def printBanner():
   print(colorize(header,'pink'))
-  print(colorize('v0.0.2 by sanre','green'))
+  print(colorize('v0.0.3 by sanre','green'))
 
 def clearPrompt():
    print("\x1B[2J")
@@ -102,6 +112,7 @@ def clearPrompt():
 def readInstances():
   wl = open("instances.txt","r")
   content = wl.readlines()
+  #print(content)      # DEBUG print
   wl.close()
   return content
 
@@ -128,10 +139,34 @@ def doTheNeedful(q, sweepMode):
   else:
     if enrichedMode == True: enrichedSearch(q, instance,timewindow)
     else: processSearch(q, instance,timewindow)
+
+def feedSearch(sweepMode):
+  socfeed = open(args.f, "r", encoding="ascii")
+  socfeed = socfeed.read()
+  socfeed = json.loads(socfeed)
+  translated_queries = []
   
-  input(colorize('Press enter to continue.', 'blue'))
-  clearPrompt()
-  mainMenu()
+  for i in socfeed['reports']:
+    report = ['title', 'query', 'description']
+    title = i['title']
+    ioc_queries = i['iocs']['query']
+    description = "description_placeholder"
+    for query in ioc_queries:
+      q = unquote(query['search_query']).split("q=")[1]
+      
+    if q != "":
+      report[0] = title
+      report[1] = q
+      report[2] = description
+      translated_queries.append(report)
+  
+  #print(translated_queries)
+
+  # For now, just go through the queries
+  # In future, add also the title and description in the output
+  for report in translated_queries:
+    print(report[0])
+    doTheNeedful(report[1], sweepMode)  
 
 def freeSearch(sweepMode):
   clearPrompt()
@@ -164,6 +199,7 @@ def mainMenu(sweepMode=sweepMode):
           elif b == "menu_powershell"   : initMenu(menu_powershell, sweepMode)
           elif b == "menu_emotet"       : initMenu(menu_emotet, sweepMode)
           elif b == "menu_lolbins"      : initMenu(menu_lolbins, sweepMode)
+          elif b == "menu_hafnium"      : initMenu(menu_hafnium, sweepMode)
           elif b == "free_search"       : freeSearch(sweepMode)
           elif b == "toggle_sweep"      : sweepMode^=True
     except (ValueError, IndexError):
@@ -208,7 +244,7 @@ def processSearch(q, instance, timewindow):
     headers = {'X-Auth-Token': apikey, 'Content-Type': 'application/json', 'Accept': 'application/json, text/plain, */*'}
 
     job_data = {
-        "fields": ["*", "process_start_time", "process_cmdline","process_internal_name","netconn_domain", "process_product_name", "crossproc_target", "crossproc_api, event_id"],
+        "fields": ["*", "process_start_time", "process_cmdline","process_internal_name","netconn_domain", "process_product_name", "crossproc_target", "crossproc_api","netconn_domain", "netconn_ipv4", "netconn_ipv6", "netconn_port", "netconn_inbound", "event_id", "childproc_name", "childproc_cmdline", "event_attack_stage", "ttp", "parent_name", "scriptload_hash", "scriptload_name", "scriptload_count", "fileless_scriptload_cmdline", "fileless_scriptload_cmdline_length", "fileless_scriptload_hash", "filemod_name"],
         "query": "device_name:{0} AND ({1})".format(device_name, q), 
         "rows": 500,
         "sort": [
@@ -222,7 +258,8 @@ def processSearch(q, instance, timewindow):
         }
     }
 
-    job_response = requests.post("{0}api/investigate/v2/orgs/{1}/processes/search_jobs".format(cbdurl,orgkey),headers=headers,data=json.dumps(job_data), verify=True)
+    job_response = requests.post("{0}api/investigate/v2/orgs/{1}/processes/search_jobs".format(cbdurl,orgkey),headers=headers,data=json.dumps(job_data), verify=True, proxies=proxies)
+    print(proxies)
 
     try:
       job_id = json.loads(job_response.text)['job_id']
@@ -231,27 +268,28 @@ def processSearch(q, instance, timewindow):
       pass
     except KeyError:
       pass
-    ## RETURNS
-    # {"job_id":"8822cdb6-bb9e-42b3-b553-142deae8be3b"}
-    ## Then retrieve results for the enriched event aggregation search
-    # job_result = requests.get(cbdurl + "api/investigate/v1/orgs/"+orgkey+"/enriched_events/aggregation_jobs/"+job_id+"/results",headers=headers)
+
     if job_id:
       for retry in range(0,12):
-        job_result = requests.get("{0}api/investigate/v2/orgs/{1}/processes/search_jobs/{2}/results".format(cbdurl,orgkey,job_id),headers=headers) # This query schema fetches 10 rows of results by default, max is 10k. End results most likely need to be paginated.
+        job_result = requests.get("{0}api/investigate/v2/orgs/{1}/processes/search_jobs/{2}/results".format(cbdurl,orgkey,job_id),headers=headers, verify=True, proxies=proxies) # This query schema fetches 10 rows of results by default, max is 10k. End results most likely need to be paginated.
         job_result_done = json.loads(job_result.text)
         contacted = job_result_done['contacted']
         completed = job_result_done['completed']
         num_found = job_result_done['num_found']
         num_available = job_result_done['num_available']
+        #print(job_result_done)
+        #print(retry)
         if (completed < contacted) and (retry <= 10):
+          #print(job_result_done)
           print("instance: {0}, num_available: {1}, num_found: {2}, completed: {3}, contacted: {4}, retry: {5}/10".format(instance.strip(), num_available, num_found, completed, contacted, retry))
+          #print(retry)
           sleep(1)
         elif retry > 10: 
           # Handle the pagination of results
           rows = 0
           print("instance: {0}, num_available: {1}, num_found: {2}, completed: {3}, contacted: {4}".format(instance.strip(), num_available, num_found, completed, contacted))
           while rows < num_available:
-            job_result = requests.get("{0}api/investigate/v2/orgs/{1}/processes/search_jobs/{2}/results?start={3}&rows=500".format(cbdurl,orgkey,job_id,rows),headers=headers) # This query schema fetches 500 rows at a time. Go through the results until pages run out.
+            job_result = requests.get("{0}api/investigate/v2/orgs/{1}/processes/search_jobs/{2}/results?start={3}&rows=500".format(cbdurl,orgkey,job_id,rows),headers=headers, verify=True, proxies=proxies) # This query schema fetches 500 rows at a time. Go through the results until pages run out.
             job_result_done = json.loads(job_result.text)
             for result in job_result_done['results']:
               result['instance'] = instance.strip()
@@ -267,11 +305,21 @@ def processSearch(q, instance, timewindow):
                 event_id = "N/A"
                 result['link_process'] = "N/A"
                 link_process = "N/A"
+                try:
+                  process_guid = result['process_guid']
+                  result['link_process'] = '{0}cb/investigate/events/events?query=device_id%3A{1}%20process_guid%3A{2}'.format(cbdurl, device_id, process_guid)
+                  link_process = result['link_process']
+                except:
+                  pass
                 pass
               
               process_start_time = result['process_start_time']
               device_name = result['device_name']
-              process_username = result['process_username']
+              try:
+                process_username = result['process_username']
+              except:
+                process_username = "N/A"
+                pass
               try:
                 process_cmdline = result['process_cmdline']
               except Exception as e:
@@ -287,14 +335,17 @@ def processSearch(q, instance, timewindow):
               for result in job_result_done['results']:
                 process_start_time = result['process_start_time']
                 device_name = result['device_name']
-                process_username = result['process_username']
+                try:
+                  process_username = result['process_username']
+                except:
+                  process_username = "N/A"
+                  pass
                 try:
                   process_cmdline = result['process_cmdline']
                 except Exception as e:
                   process_cmdline = "N/A"
                   pass
                 link_process = result['link_process']
-                #link_process = "ph"
                 print("{0} {1} {2} {3} {4} \n\033[1;30;40m{5}\033[m".format(process_start_time, instance.strip(), device_name, process_username, process_cmdline, link_process))
               break
 
@@ -317,7 +368,7 @@ def enrichedSearch(q, instance, timewindow):
     headers = {'X-Auth-Token': apikey, 'Content-Type': 'application/json', 'Accept': 'application/json, text/plain, */*'}
 
     job_data = {
-        "fields": ["*", "process_start_time", "process_cmdline","process_internal_name","netconn_domain", "process_product_name", "crossproc_target", "crossproc_api"],
+        "fields": ["*", "process_start_time", "process_cmdline","process_internal_name","netconn_domain", "process_product_name", "crossproc_target", "crossproc_api","netconn_domain", "netconn_ipv4", "netconn_ipv6", "netconn_port", "netconn_inbound", "event_id", "childproc_name", "childproc_cmdline", "event_attack_stage", "ttp", "parent_name", "scriptload_hash", "scriptload_name", "scriptload_count", "fileless_scriptload_cmdline", "fileless_scriptload_cmdline_length", "fileless_scriptload_hash"],
         "query": "device_name:{0} AND ({1})".format(device_name, q), 
         "rows": 500,
         "sort": [
@@ -331,7 +382,7 @@ def enrichedSearch(q, instance, timewindow):
         }
     }
 
-    job_response = requests.post("{0}api/investigate/v1/orgs/{1}/enriched_events/aggregation_jobs/device_id".format(cbdurl,orgkey),headers=headers,data=json.dumps(job_data), verify=True)
+    job_response = requests.post("{0}api/investigate/v1/orgs/{1}/enriched_events/aggregation_jobs/device_id".format(cbdurl,orgkey),headers=headers,data=json.dumps(job_data), verify=True, proxies=proxies)
 
     try:
       job_id = json.loads(job_response.text)['job_id']
@@ -340,13 +391,9 @@ def enrichedSearch(q, instance, timewindow):
       pass
     except KeyError:
       pass
-    ## RETURNS
-    # {"job_id":"8822cdb6-bb9e-42b3-b553-142deae8be3b"}
-    ## Then retrieve results for the enriched event aggregation search
-    # job_result = requests.get(cbdurl + "api/investigate/v1/orgs/"+orgkey+"/enriched_events/aggregation_jobs/"+job_id+"/results",headers=headers)
     if job_id:
       for retry in range(0,12):
-        job_result = requests.get("{0}api/investigate/v1/orgs/{1}/enriched_events/aggregation_jobs/{2}/results".format(cbdurl,orgkey,job_id),headers=headers) # This query schema fetches 10 rows of results by default, max is 10k. End results most likely need to be paginated.
+        job_result = requests.get("{0}api/investigate/v1/orgs/{1}/enriched_events/aggregation_jobs/{2}/results".format(cbdurl,orgkey,job_id),headers=headers, verify=True, proxies=proxies) # This query schema fetches 10 rows of results by default, max is 10k. End results most likely need to be paginated.
         job_result_done = json.loads(job_result.text)
         contacted = job_result_done['contacted']
         completed = job_result_done['completed']
@@ -361,7 +408,7 @@ def enrichedSearch(q, instance, timewindow):
           rows = 0
           print("instance: {0}, num_available: {1}, num_found: {2}, completed: {3}, contacted: {4}".format(instance.strip(), num_available, num_found, completed, contacted))
           while rows < num_available:
-            job_result = requests.get("{0}api/investigate/v1/orgs/{1}/enriched_events/aggregation_jobs/{2}/results?start={3}&rows=500".format(cbdurl,orgkey,job_id,rows),headers=headers) # This query schema fetches 500 rows at a time. Go through the results until pages run out.
+            job_result = requests.get("{0}api/investigate/v1/orgs/{1}/enriched_events/aggregation_jobs/{2}/results?start={3}&rows=500".format(cbdurl,orgkey,job_id,rows),headers=headers, verify=True, proxies=proxies) # This query schema fetches 500 rows at a time. Go through the results until pages run out.
             job_result_done = json.loads(job_result.text)
             for result in job_result_done['results']:
               result['instance'] = instance.strip()
@@ -395,4 +442,6 @@ def enrichedSearch(q, instance, timewindow):
 
 if args.i == True:
   mainMenu(sweepMode)
+elif args.f:
+  feedSearch(sweepMode)
 else: freeSearch(sweepMode)
